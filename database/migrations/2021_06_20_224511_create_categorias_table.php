87a93519199bf772037d85375bdc77850d61c236
cheverriaw <willytpi2021@gmail.com>
2021-06-20 17:09:06 -0600

    cree la migracion categorias

--- database/migrations/2021_06_20_224511_create_categorias_table.php
+++ database/migrations/2021_06_20_224511_create_categorias_table.php
@@ -15,6 +15,9 @@ class CreateCategoriasTable extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre', 50);
+            $table->text('descripcion');
+            $table->boolean('activo')->nullable()->default(false);
             $table->timestamps();
         });
     }
