b619a38f5c6c4761f910f031ac1ac165c9639ce0
cheverriaw <willytpi2021@gmail.com>
2021-06-20 17:15:14 -0600

    cree migracion clientes

--- database/migrations/2021_06_20_224553_create_clientes_table.php
+++ database/migrations/2021_06_20_224553_create_clientes_table.php
@@ -15,6 +15,17 @@ class CreateClientesTable extends Migration
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
+            $table->string('nombres', 50);
+            $table->string('apellidos', 50);
+            $table->string('identificacion', 50)->unique();
+            $table->string('direccion', 50);
+            $table->string('estado', 50);
+            $table->string('pais', 50);
+            $table->string('telefono', 50);
+            $table->string('email', 50)->unique();
+            $table->string('tarjeta_de_credito', 50)->unique();
+            $table->string('usuario', 50)->unique();
+            $table->string('password', 50);
             $table->timestamps();
         });
     }
