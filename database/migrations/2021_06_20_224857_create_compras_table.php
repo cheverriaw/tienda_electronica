ca5e4af1bc4cdcab94a4d4c42b6af9ee5348bcec
cheverriaw <willytpi2021@gmail.com>
2021-06-20 17:49:00 -0600

    cree la migracion compras

--- database/migrations/2021_06_20_224857_create_compras_table.php
+++ database/migrations/2021_06_20_224857_create_compras_table.php
@@ -15,6 +15,20 @@ class CreateComprasTable extends Migration
     {
         Schema::create('compras', function (Blueprint $table) {
             $table->id();
+            //relacionamos con el cliente
+            $table->unsignedBigInteger('id_cliente')->nullable();
+            $table->foreign('id_cliente')->references('id')->on('clientes')->onDelete('set null');
+
+            //relacionamo carritos
+            $table->unsignedBigInteger('id_carrito')->nullable();
+            $table->foreign('id_carrito')->references('id')->on('carritos')->onDelete('set null');
+             
+            //relacionamo pagos
+            $table->unsignedBigInteger('id_pago')->nullable();
+            $table->foreign('id_pago')->references('id')->on('pagos')->onDelete('set null');
+
+
+            $table->decimal('total', 8, 2);
             $table->timestamps();
         });
     }
